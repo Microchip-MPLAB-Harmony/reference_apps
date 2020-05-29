63b340c54a26febbc50b0f6a2d5fc51f69c7538d
Thimmarayaswamy Krishnappa <Thimmarayaswamy.Krishnappa@microchip.com>
2020-05-29 15:22:36 +0530

    [APPS_DOC] Regenerated and tested PIC32MZEF audio_player_lab1 application and updated readme.md
    M32MM-303
    
    Signed-off-by: Thimmarayaswamy Krishnappa <Thimmarayaswamy.Krishnappa@microchip.com>

--- apps/pic32mz_ef_sk_meb2/audio_player/audio_player_lab1/firmware/src/config/default/tasks.c
+++ apps/pic32mz_ef_sk_meb2/audio_player/audio_player_lab1/firmware/src/config/default/tasks.c
@@ -55,6 +55,7 @@
 
 
 
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: System "Tasks" Routine
@@ -84,6 +85,7 @@ void SYS_Tasks ( void )
     /* Maintain the application's state machine. */
         /* Call Application task APP. */
     APP_Tasks();
+
 
 
 
