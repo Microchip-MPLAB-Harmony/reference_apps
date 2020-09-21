59bfaf824c2bd26f2cb5ac84c9c7c0be223d30b5
santosh <santosh.tangudu@microchip.com>
2020-09-21 21:07:04 +0530

    [DFT] Regenerated and tested Curiosity PIC32MZ EF 2.0 getting_started_ext application
    M32MM-623

--- apps/pic32mz_ef_curiosity_v2/getting_started_ext/firmware/src/config/pic32mz_curiosity2/peripheral/uart/plib_uart6.c
+++ apps/pic32mz_ef_curiosity_v2/getting_started_ext/firmware/src/config/pic32mz_curiosity2/peripheral/uart/plib_uart6.c
@@ -85,14 +85,15 @@ void UART6_Initialize( void )
     /* Set up UxMODE bits */
     /* STSEL  = 0 */
     /* PDSEL = 0 */
+    /* UEN = 0 */
 
-    U6MODE = 0x0;
+    U6MODE = 0x8;
 
     /* Enable UART6 Receiver and Transmitter */
-    U6STASET = (_U6STA_UTXEN_MASK | _U6STA_URXEN_MASK);
+    U6STASET = (_U6STA_UTXEN_MASK | _U6STA_URXEN_MASK | _U6STA_UTXISEL1_MASK);
 
     /* BAUD Rate register Setup */
-    U6BRG = 53;
+    U6BRG = 216;
 
     /* Turn ON UART6 */
     U6MODESET = _U6MODE_ON_MASK;
@@ -101,33 +102,40 @@ void UART6_Initialize( void )
 bool UART6_SerialSetup( UART_SERIAL_SETUP *setup, uint32_t srcClkFreq )
 {
     bool status = false;
-    uint32_t baud = setup->baudRate;
-    uint32_t brgValHigh = 0;
-    uint32_t brgValLow = 0;
+    uint32_t baud;
+    int32_t brgValHigh = 0;
+    int32_t brgValLow = 0;
     uint32_t brgVal = 0;
     uint32_t uartMode;
 
     if (setup != NULL)
     {
+        baud = setup->baudRate;
+
+        if (baud == 0)
+        {
+            return status;
+        }
+
         if(srcClkFreq == 0)
         {
             srcClkFreq = UART6_FrequencyGet();
         }
 
         /* Calculate BRG value */
-        brgValLow = ((srcClkFreq / baud) >> 4) - 1;
-        brgValHigh = ((srcClkFreq / baud) >> 2) - 1;
+        brgValLow = (((srcClkFreq >> 4) + (baud >> 1)) / baud ) - 1;
+        brgValHigh = (((srcClkFreq >> 2) + (baud >> 1)) / baud ) - 1;
 
         /* Check if the baud value can be set with low baud settings */
-        if((brgValHigh >= 0) && (brgValHigh <= UINT16_MAX))
+        if((brgValLow >= 0) && (brgValLow <= UINT16_MAX))
         {
-            brgVal =  (((srcClkFreq >> 2) + (baud >> 1)) / baud ) - 1;
-            U6MODESET = _U6MODE_BRGH_MASK;
-        }
-        else if ((brgValLow >= 0) && (brgValLow <= UINT16_MAX))
-        {
-            brgVal = ( ((srcClkFreq >> 4) + (baud >> 1)) / baud ) - 1;
+            brgVal =  brgValLow;
             U6MODECLR = _U6MODE_BRGH_MASK;
+        }
+        else if ((brgValHigh >= 0) && (brgValHigh <= UINT16_MAX))
+        {
+            brgVal = brgValHigh;
+            U6MODESET = _U6MODE_BRGH_MASK;
         }
         else
         {
