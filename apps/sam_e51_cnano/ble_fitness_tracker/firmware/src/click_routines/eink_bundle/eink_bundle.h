4243f12c20f1d0285a45657f9c485b13cdbd94f0
santosh <santosh.tangudu@microchip.com>
2021-05-25 15:35:36 +0530

    [APPS] Updated SAM E51 CNano BLE fitness tracker demo with respect to the EINK click routine changes
    M32MM-1134

--- apps/sam_e51_cnano/ble_fitness_tracker/firmware/src/click_routines/eink_bundle/eink_bundle.h
+++ apps/sam_e51_cnano/ble_fitness_tracker/firmware/src/click_routines/eink_bundle/eink_bundle.h
@@ -56,8 +56,8 @@ You should have received a copy of the GNU General Public License along with
 this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 *******************************************************************************/
-#ifndef EINK_HW_H_
-#define EINK_HW_H_
+#ifndef _EINK_BUNDLE_H_
+#define _EINK_BUNDLE_H_
 
 #include <stdint.h>
 
