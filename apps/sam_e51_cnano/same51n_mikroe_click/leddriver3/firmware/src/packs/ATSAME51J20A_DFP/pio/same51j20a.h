1b471d39c46b2830ffd4e542d643913e364b92ee
Mohan Kumar RP <mohan.kumar@microchip.com>
2021-06-03 17:58:31 +0530

    [SAM_E51_CNANO] Regenerated demo for same51n_mikroe_click_leddriver3
    M32MM-1169
    
    Signed-off-by: Mohan Kumar RP <mohan.kumar@microchip.com>

--- apps/sam_e51_cnano/same51n_mikroe_click/leddriver3/firmware/src/packs/ATSAME51J20A_DFP/pio/same51j20a.h
+++ apps/sam_e51_cnano/same51n_mikroe_click/leddriver3/firmware/src/packs/ATSAME51J20A_DFP/pio/same51j20a.h
@@ -1,7 +1,7 @@
 /**
  * \brief Peripheral I/O description for SAME51J20A
  *
- * Copyright (c) 2020 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2021 Microchip Technology Inc. and its subsidiaries.
  *
  * Subject to your compliance with these terms, you may use Microchip software and any derivatives
  * exclusively with Microchip products. It is your responsibility to comply with third party license
@@ -20,7 +20,7 @@
  *
  */
 
-/* file generated from device description version 2020-06-22T15:30:33Z */
+/* file generated from device description version 2020-11-23T12:27:58Z */
 #ifndef _SAME51J20A_GPIO_H_
 #define _SAME51J20A_GPIO_H_
 
