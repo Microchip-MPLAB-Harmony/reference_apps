247d49d9b29dd821665a44b6b61b1787b310a2e6
santosh <santosh.tangudu@microchip.com>
2021-06-04 15:00:21 +0530

    [SAM_E51_CNANO] Regenerated demo for same51n_mikroe_click_eink_bundle
    M32MM-1195
    
    Signed-off-by: santosh <santosh.tangudu@microchip.com>

--- apps/sam_e51_cnano/same51n_mikroe_click/eink_bundle/firmware/src/click_routines/eink_bundle/eink_bundle_font.c
+++ apps/sam_e51_cnano/same51n_mikroe_click/eink_bundle/firmware/src/click_routines/eink_bundle/eink_bundle_font.c
@@ -12,21 +12,30 @@
 * Plot No. 1498, EPIP, 1st Phase Industrial Area, Whitefield, Bengaluru,
 * Karnataka 560066
 *******************************************************************************/
+// DOM-IGNORE-BEGIN
 /*******************************************************************************
- Copyright (C) 2017  Microchip Technology Incorporated and its subsidiaries.
-
-This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation; either version 2 of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with
-this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+* Copyright (C) 2020 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
 *******************************************************************************/
+// DOM-IGNORE-END
 
 #include "eink_bundle_font.h"
 
