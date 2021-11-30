f2fe920197e057e21773b412f1d4f99f07fab933
Mohan Kumar RP <mohan.kumar@microchip.com>
2021-11-30 16:45:40 +0530

    [SAM_D21_CNANO] Regenerated and tested fitness_tracker demo
    Added MCC Support
    
    M32MM-1294
    M32MM-1370
    
    Signed-off-by: Mohan Kumar RP <mohan.kumar@microchip.com>

--- apps/sam_d21_cnano/fitness_tracker/firmware/src/config/sam_d21_cnano/peripheral/port/plib_port.c
+++ apps/sam_d21_cnano/fitness_tracker/firmware/src/config/sam_d21_cnano/peripheral/port/plib_port.c
@@ -75,10 +75,12 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_DIR = 0x40080;
    PORT_REGS->GROUP[0].PORT_OUT = 0x40080;
    PORT_REGS->GROUP[0].PORT_PINCFG[6] = 0x2;
+   PORT_REGS->GROUP[0].PORT_PINCFG[7] = 0x0;
    PORT_REGS->GROUP[0].PORT_PINCFG[8] = 0x1;
    PORT_REGS->GROUP[0].PORT_PINCFG[9] = 0x1;
    PORT_REGS->GROUP[0].PORT_PINCFG[16] = 0x1;
    PORT_REGS->GROUP[0].PORT_PINCFG[17] = 0x1;
+   PORT_REGS->GROUP[0].PORT_PINCFG[18] = 0x0;
    PORT_REGS->GROUP[0].PORT_PINCFG[19] = 0x1;
    PORT_REGS->GROUP[0].PORT_PINCFG[22] = 0x1;
 
@@ -90,8 +92,11 @@ void PORT_Initialize(void)
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x800600;
    PORT_REGS->GROUP[1].PORT_OUT = 0x800e00;
+   PORT_REGS->GROUP[1].PORT_PINCFG[9] = 0x0;
+   PORT_REGS->GROUP[1].PORT_PINCFG[10] = 0x0;
    PORT_REGS->GROUP[1].PORT_PINCFG[11] = 0x5;
    PORT_REGS->GROUP[1].PORT_PINCFG[22] = 0x1;
+   PORT_REGS->GROUP[1].PORT_PINCFG[23] = 0x0;
 
    PORT_REGS->GROUP[1].PORT_PMUX[11] = 0x3;
 
@@ -306,7 +311,7 @@ void PORT_PinPeripheralFunctionConfig(PORT_PIN pin, PERIPHERAL_FUNCTION function
     PORT_GROUP group = GET_PORT_GROUP(pin);
     uint32_t pin_num = ((uint32_t)pin) & 0x1FU;
     uint32_t pinmux_val = (uint32_t)((port_group_registers_t*)group)->PORT_PMUX[(pin_num >> 1)];
-    
+
     /* For odd pins */
     if (0U != (pin_num & 0x01U))
     {
@@ -317,7 +322,7 @@ void PORT_PinPeripheralFunctionConfig(PORT_PIN pin, PERIPHERAL_FUNCTION function
         pinmux_val = (pinmux_val & ~0x0FU) | periph_func;
     }
     ((port_group_registers_t*)group)->PORT_PMUX[(pin_num >> 1)] = (uint8_t)pinmux_val;
-    
+
     /* Enable peripheral control of the pin */
     ((port_group_registers_t*)group)->PORT_PINCFG[pin_num] |= (uint8_t)PORT_PINCFG_PMUXEN_Msk;
 }
