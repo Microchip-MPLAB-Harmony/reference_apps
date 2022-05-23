97b3f0487c65052756717479d64d67e1f236aed7
Mohan Kumar RP <mohan.kumar@microchip.com>
2022-05-23 15:14:48 +0530

    [APP_DOCS] Regenerated and tested ble weather station samd21_cnano application
    M32MM-1626
    
    Signed-off-by: Mohan Kumar RP <mohan.kumar@microchip.com>

--- apps/sam_d21_cnano/ble_weather_station/firmware/src/config/samd21_cnano/definitions.h
+++ apps/sam_d21_cnano/ble_weather_station/firmware/src/config/samd21_cnano/definitions.h
@@ -50,8 +50,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include "peripheral/sercom/usart/plib_sercom3_usart.h"
-#include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/sercom/i2c_master/plib_sercom2_i2c_master.h"
+#include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/evsys/plib_evsys.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
