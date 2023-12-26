680599c47f49a23d096082f7197ca6dd6ccaf854
Sourav S Pasarad <sourav.spasarad@microchip.com>
2023-12-26 16:12:02 +0530

    [APP_DOCS] Created a RNBD451 Basic Data Exchange Applicaton demo on SAM E51 Cnano and it's documentation
    M32MM-2162
    
    Signed-off-by: Sourav S Pasarad <sourav.spasarad@microchip.com>

---
+++ apps/sam_e51_cnano/same51n_mikroe_click/rnbd451/firmware/src/packs/ATSAME51J20A_DFP/instance/gclk.h
@@ -0,0 +1,41 @@
+/*
+ * Instance header file for ATSAME51N20A
+ *
+ * Copyright (c) 2023 Microchip Technology Inc. and its subsidiaries.
+ *
+ * Subject to your compliance with these terms, you may use Microchip software and any derivatives
+ * exclusively with Microchip products. It is your responsibility to comply with third party license
+ * terms applicable to your use of third party software (including open source software) that may
+ * accompany Microchip software.
+ *
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY,
+ * APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, INCIDENTAL OR CONSEQUENTIAL
+ * LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF
+ * MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE FULLEST EXTENT
+ * ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT
+ * EXCEED THE AMOUNT OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ */
+
+/* file generated from device description version 2023-03-17T09:49:56Z */
+#ifndef _SAME51_GCLK_INSTANCE_
+#define _SAME51_GCLK_INSTANCE_
+
+
+/* ========== Instance Parameter definitions for GCLK peripheral ========== */
+#define GCLK_GENCTRL0_RESETVALUE                 (106)      /* Default specific reset value for generator 0 */
+#define GCLK_GENDIV_BITS                         (16)       
+#define GCLK_GEN_BITS                            (4)        
+#define GCLK_GEN_NUM                             (12)       /* Number of Generic Clock Generators */
+#define GCLK_GEN_NUM_MSB                         (11)       /* Number of Generic Clock Generators - 1 */
+#define GCLK_GEN_SOURCE_NUM_MSB                  (8)        /* Number of Generic Clock Sources - 1 */
+#define GCLK_INSTANCE_ID                         (7)        /* Instance index for GCLK */
+#define GCLK_IO_NUM                              (8)        /* Number of Generic Clock I/Os */
+#define GCLK_NUM                                 (48)       /* Number of Generic Clock Users */
+#define GCLK_SOURCE_BITS                         (4)        
+#define GCLK_SOURCE_NUM                          (9)        /* Number of Generic Clock Sources */
+
+#endif /* _SAME51_GCLK_INSTANCE_ */
